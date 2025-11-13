a3a479d847c40f0539124afadba2a0b41705a640
liu-han-66 <3289038386@qq.com>
2025-11-13 21:47:22 +0800

    冒泡排序

--- practice11.12/practice11.12/11.12.c
+++ practice11.12/practice11.12/11.12.c
@@ -175,14 +175,38 @@
 //	printf("%d", min);
 //	return 0;
 //}
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1 };
+//	//½øĞĞÉıĞò
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int min = arr[0];
+//	
+//	return 0;
+//}
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int arr[] = { 9,8,7,6,5,4,3,2,1 };
-	//½øĞĞÉıĞò
-	int i = 0;
+	//Ã°ÅİÅÅĞò
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	int min = arr[0];
-	
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i + 1] = temp;
+			}
+		}
+		printf("%d ", arr[i]);
+	}
 	return 0;
 }
