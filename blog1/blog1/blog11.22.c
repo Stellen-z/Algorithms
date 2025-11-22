c6cbf65b493558b9d6d39b0458916b17cb33f5cf
liu-han-66 <3289038386@qq.com>
2025-11-22 23:54:03 +0800

    blog日志

---
+++ blog1/blog1/blog11.22.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//int main()
+//{
+//	//¥Ú”°1-10
+//	int i;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	//¥Ú”°1-10
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	//¥Ú”°1-10
+	int i = 1;
+	do
+	{
+		printf("%d ", i);
+		i++;
+	} while (i <= 10);
+	return 0;
+}
