1ce9160fb78fa99c76e3daaea1454dfe4b9a0b73
liu-han-66 <3289038386@qq.com>
2025-11-23 22:15:07 +0800

    作业练习

--- blog1/blog1/blog11.22.c
+++ blog1/blog1/blog11.22.c
@@ -1,12 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+#include <stdio.h>
+int main()
+{
+	//打印1-10
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+		{
+			continue;
+		}
+		printf("%d ", i);
+	}
+	return 0;
+}
+//#include <stdio.h>
 //int main()
 //{
 //	//打印1-10
-//	int i;
-//	for (i = 1; i <= 10; i++)
+//	int i = 1;
+//	while (i <= 10)
 //	{
+//		if (i == 5)
+//		{
+//			//break;
+//			continue;
+//		}
 //		printf("%d ", i);
+//		i++;
 //	}
 //	return 0;
 //}
@@ -15,22 +36,10 @@
 //{
 //	//打印1-10
 //	int i = 1;
-//	while (i <= 10)
+//	do
 //	{
 //		printf("%d ", i);
 //		i++;
-//	}
+//	} while (i <= 10);
 //	return 0;
 //}
-#include <stdio.h>
-int main()
-{
-	//打印1-10
-	int i = 1;
-	do
-	{
-		printf("%d ", i);
-		i++;
-	} while (i <= 10);
-	return 0;
-}
