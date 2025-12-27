1b1e6af8cb6772ff2b5aeadf515adc5091492147
liu-han-66 <3289038386@qq.com>
2025-12-27 22:59:35 +0800

    第一次跟练Cpp

---
+++ Day01/Day01/lesson01.cpp
@@ -0,0 +1,239 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char ch = 'q';
+//	cout << ch << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char ch = 'w';
+//	short s = 2;
+//	short int = 3;
+//	int num = 100;
+//	long l = 20;
+//	long long t = 1000;
+//	double d1 = 3.5;
+//	float f1 = 3.0f;
+//	
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	double d1 = 3.14e2;
+//	cout << d1 << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	bool flag = true;
+//	if(flag)
+//	{
+//		cout << "I like C++" << endl;
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <climits>
+//using namespace std;
+//int main()
+//{
+//	cout << SHRT_MIN << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+////	int a = 100;
+////	cout << sizeof(a) << endl;
+////	cout << sizeof a << endl;
+//    cout << sizeof(char) << endl;
+//    cout << sizeof(bool) << endl;
+//    cout << sizeof(short) << endl;
+//    cout << sizeof(int) << endl;
+//    cout << sizeof(long) << endl;
+//    cout << sizeof(long long) << endl;
+//    cout << sizeof(float) << endl;
+//    cout << sizeof(double) << endl;
+//	cout << sizeof(long double) << endl;
+//	return 0;
+//}
+
+
+//#include <iostream>
+//#include <climits>
+//using namespace std;
+//int main()
+//{
+//	cout << LONG_MAX << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	short s;
+//	cout << sizeof(n) << " " << sizeof(s) << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char ch;
+//	int num;
+//	float f1;
+//	double d1;
+//	
+//	cout << "char:" << ch << endl;
+//	cout << "int:" << num << endl;
+//	cout << "float:" << f1 << endl;
+//	cout << "double:" << d1 << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//#define M 100
+//int main()
+//{
+//	cout << M << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//const int PI = 3.14159;
+//int main()
+//{
+//	int r = 0;
+//	cin >> r;
+//	cout << "周长：" << 2*PI*r << endl; 
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int x = 0;//兔子个数
+//    int y = 0;//鸡的个数
+//    int h = 35;//头的个数
+//    int f = 94;//脚的个数
+//
+//    y = (4*h - f)/2;
+//    x = h - y;
+//
+//    cout << x << y << endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int a,b,c,d;
+//    cin >> a >> b >> c >> d;
+//    int x = a * 60 + b;
+//    int y = c * 60 + d;
+//    cout << (x-y)/60 << " " << (x-y)%60 << endl;
+//    return 0;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a,b;
+//	cin >> a >> b; 
+//	int tmp = a;
+//	a = b;
+//	b = tmp;
+//	cout << a << " " << b << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int a;
+//    cin >> a;
+//    cout << (char)a << endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int ch = getchar();
+//	cout << ch << endl;
+//	cout << ch;
+//	cout << "xxxx" << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int ch = getchar();
+//	cout << ch << endl;
+//	cout << (char)ch << endl;
+//	cout << ch << endl;
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int ch = getchar();
+	putchar(ch);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
