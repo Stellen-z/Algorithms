27a2d528514c5399f9a4f6cdd374d7a792ee1dc0
lzh65038-web <lzh65038@gmail.com>
2026-06-01 23:05:16 +0800

    6.1
    更新Contest

--- gitte/no.1/test.1.26/test.1.26/1.26.cpp
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "hello world!" << endl;
-	return 0;
-}
--- test_1.22/test_1.22/TEST.1.22.cpp
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "yes" << endl;
-	return 0;
-}
