3f2db093788a5dbcf8bfe6ddbba1b9d9f7335117
lzh65038-web <lzh65038@gmail.com>
2026-07-14 22:11:17 +0800

    铺地毯
    枚举--铺地毯

---
+++ Basic_Algorithms/Enumeration/Carpet_Laying/Carpet_Laying/Carpet_Laying/Carpet_Laying.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 1e4 + 10;
+
+int n;
+int a[N], b[N], g[N], k[N];
+int x, y;
+
+int GetCarpetNums()
+{
+	cin >> x >> y;
+
+	//逆序遍历即可
+	for (int i = n; i >= 0; i--)
+	{
+		if (a[i] <= x && x <= a[i] + g[i] && b[i] <= y && y <= b[i] + k[i])
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> a[i] >> b[i] >> g[i] >> k[i];
+
+	cout << GetCarpetNums() << endl;
+
+	return 0;
+}
