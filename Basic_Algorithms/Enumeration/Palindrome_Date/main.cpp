e688cd4c46d31d7bf7399be50cd4bcf774bb773c
lzh65038-web <lzh65038@gmail.com>
2026-07-14 22:13:23 +0800

    回文日期
    枚举--回文日期

---
+++ Basic_Algorithms/Enumeration/Palindrome_Date/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int x,y;
+
+int day[] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+int cnt;
+
+int main()
+{
+	cin >> x >> y;
+	
+	//按月日筛选
+	for(int i = 1;i <= 12;i++)
+	{
+		for(int j = 0;j <= day[i];j++)
+		{
+			//月日
+			int p = i * 100 + j;
+			//年份
+			int q = (j % 10) * 1000 + (j / 10) * 100 + (i % 10) * 10 + i / 10;
+			
+			//日期
+			int date = q * 10000 + p;
+			
+			if(x <= date && date <= y) cnt++; 
+		}
+	} 
+	
+	cout << cnt << endl;
+	return 0;
+} 
