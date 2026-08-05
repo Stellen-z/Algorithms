86e2aa7ea8bf37bd6ed82559da76a03e1b7b4719
lzh65038-web <lzh65038@gmail.com>
2026-08-05 21:38:43 +0800

    地毯
    地毯

---
+++ Basic_Algorithms/Difference/carpet.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1010;
+
+int f[N][N];
+
+void difference(int x1,int y1,int x2,int y2,int k)
+{
+	f[x1][y1] += k;
+	f[x1][y2+1] -= k;
+	f[x2+1][y1] -= k;
+	f[x2+1][y2+1] += k;
+}
+
+
+int main()
+{
+	int n,m;
+	cin >> n >> m;
+	
+	while(m--)
+	{
+		int x1,y1,x2,y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		
+		difference(x1,y1,x2,y2,1);
+	}
+	
+	//前缀和即为修改后的数组
+	for(int i = 1;i <= n;i++)
+	{
+		for(int j = 1;j <= n;j++)
+		{
+			f[i][j] = f[i-1][j] + f[i][j-1] - f[i-1][j-1] + f[i][j];
+			cout << f[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	
+	return 0;
+}
