fd51eeca3bd45f15a1cd231910827f89c8965ff8
lzh65038-web <lzh65038@gmail.com>
2026-08-11 23:07:59 +0800

    栈和队列
    栈和队列

---
+++ Basic_Algorithms/Sliding_Window/throw_the _handkerchief.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long LL;
+
+const int N = 1e5 + 10;
+
+LL a[N];
+
+int main()
+{
+	int n; cin >> n;
+	
+	LL sum = 0;
+	for(int i = 1;i <= n;i++)
+	{
+		cin >> a[i];
+		sum += a[i];
+	}
+	
+	int begin = 1,end = 1;
+	LL k = 0;
+	LL ret = 0;
+	while(end <= n)
+	{
+		//进窗口
+		k += a[end];
+		while(2 * k >= sum)
+		{
+			//更新结果
+			ret = max(ret,sum-k);
+			
+			//出窗口
+			k -= a[begin++];
+		}
+		ret = max(ret,k);
+		++end;
+		
+	}
+	
+	cout << ret << endl;
+	
+	return 0;
+}
