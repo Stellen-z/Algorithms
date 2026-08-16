102972cd84d837a5ad8c771dc4891d3ebb67a646
lzh65038-web <lzh65038@gmail.com>
2026-08-16 23:54:46 +0800

    debug
    debug

---
+++ Problems_Debug/P10452/P10452/debug.cpp
@@ -0,0 +1,79 @@
+﻿//求的是极值，而非中位数
+//中位数到左右各端点的距离才是最小
+// |a - x| + |b - x| >= |a - b|
+// x在[a,b]时，取等
+
+//#include <iostream>
+//#include <vector>
+//
+//typedef long long LL;
+//
+//using namespace std;
+//
+//int n;
+//
+//int main()
+//{
+//	cin >> n;
+//	vector<int> v(n + 1);
+//
+//	int max_val = 0;
+//	int min_val = 1e5;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> v[i];
+//		max_val = max(max_val, v[i]);
+//		min_val = min(min_val, v[i]);
+//	}
+//
+//	int mid = (max_val + min_val) / 2;
+//
+//	LL sum = 0;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		sum += abs(mid - v[i]);
+//	}
+//
+//	cout << sum << endl;
+//
+//
+//	return 0;
+//}
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+typedef long long LL;
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+	vector<int> v(n + 1);
+
+	for (int i = 1; i <= n; i++) cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	LL ret = 0;
+	//	for (int i = 1; i <= n; i++) 
+	//	{
+	//		ret += abs(v[i] - v[n / 2]);
+	//	} 
+		//用结论来求
+	for (int i = 1; i <= n / 2; i++)
+	{
+		ret += abs(v[i] - v[n + 1 - i]);
+	}
+
+	cout << ret << endl;
+
+
+	return 0;
+}
---
+++ Problems_Debug/P1115/P1115/debug.cpp
@@ -0,0 +1,71 @@
+﻿//无法处理全部为负数的情况
+//必须在sum = 0之前更新ret
+
+
+//#include <iostream>
+//#include <vector>
+//
+//typedef long long LL;
+//
+//using namespace std;
+//
+//int n;
+//
+//int main()
+//{
+//	cin >> n;
+//	vector<int> v(n + 1);
+//
+//	for (int i = 1; i <= n; i++) cin >> v[i];
+//
+//	LL ret = -2e9;
+//	LL sum = 0;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (sum + v[i] <= 0)
+//		{
+//			sum = 0;
+//		}
+//		else
+//		{
+//			sum += v[i];
+//			ret = max(ret, sum);
+//		}
+//
+//	}
+//
+//	cout << ret << endl;
+//
+//	return 0;
+//}
+
+
+#include <iostream>
+#include <vector>
+
+typedef long long LL;
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+	vector<int> v(n + 1);
+
+	for (int i = 1; i <= n; i++) cin >> v[i];
+
+	LL ret = -2e9;
+	LL sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum += v[i];
+		ret = max(ret, sum);
+		if (sum < 0) sum = 0;
+	}
+
+	cout << ret << endl;
+
+	return 0;
+}
